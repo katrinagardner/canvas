718cb271987c44f9483f576467102c7bc6bd0a7d
katrina gardner <yutzeng0314@gmail.com>
2014-11-25 20:39:12 -0500

    exercise

---
+++ hockey.js
@@ -0,0 +1,12 @@
+var hockey = function(goals,assists, points, shots){
+  this.goals = goals;
+  this.assists=assists;
+  this.points=points;
+  this.shots=shots;
+};
+
+var RickNash= new hockey(14, 7, 21, 73);
+
+console.log(RickNash);
+
+//add function under var RickNash new will be you want to do something extra beside var hockey function. 
